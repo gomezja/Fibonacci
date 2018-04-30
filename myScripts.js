5f7650ca122013947933d37fd06fa448009fc2cd
gomezja <jgomez42gb@gmail.com>
2018-04-30 12:18:02 -0700

    reorganized code

--- myScripts.js
+++ myScripts.js
@@ -28,19 +28,25 @@ submitBtn.addEventListener("click", function() {
 	document.getElementById("output").innerHTML = fibOutputStr;
 });
 
-function inputValidation(num) {
+/**
+ * input validation for text form
+ * 
+ * @param {*} val
+ * @return {boolean}
+ */
+function inputValidation(val) {
 	var isValidInt = true;
 
-	if(num < 0) { // check if num is a negative
+	if(val < 0) { // check if val is a negative
 		fibOutputStr = "Invalid Input: negative number";
 		return false;
-	} else if(num === "") { // check if num is an empty string
+	} else if(val === "") { // check if val is an empty string
 		fibOutputStr = "Invalid Input: empty field";
 	 	return false;
-	} else if(isNaN(Number(num))) { // check if num is not a number
+	} else if(isNaN(Number(val))) { // check if val is not a number
 		fibOutputStr = "Invalid Input: not a number";
 		return false;
-	} else if(num % 1 != 0) { // check if num is a decimal
+	} else if(val % 1 != 0) { // check if val is a decimal
 		fibOutputStr = "Invalid Input: decimal number";
 		return false;
 	}
@@ -48,10 +54,17 @@ function inputValidation(num) {
 	return isValidInt;
 }
 
+/**
+ * calculates the fibonnaci result of user input
+ * 
+ * @param {number} num
+ * @return {number}
+ */
 function doFibonacci(num) {
 	fibSequence = [];
 	fibSequence.push(0);
 
+	// check if num is 0 or 1
 	if(num === 0) {
 		return 0;
 	} else if(num === 1) {
@@ -67,6 +80,7 @@ function doFibonacci(num) {
 	fibSequence.push(1);
 	fibSequence.push(1);
 
+	// perform fibonacci sequence and save each iteration
 	for(var i = 3; i <= num; i++) {
 		temp = num1;
 		num1 = num2;
@@ -75,18 +89,23 @@ function doFibonacci(num) {
 		fibSequence.push(num1 + num2);
 	}
 
+	// result
 	return (num1 + num2);
 }
 
+/**
+ * creates the html table with the created fib sequence
+ */
 function createTable() {
 	//start of table
 	var tableStr = "<h2>The resulting table</h2><div class=\"results\" style=\"overflow-x:auto;\"><table border = 1px><tr>";
 
+	// table headers
 	for(var i = 0; i < fibSequence.length; i++) {
 		tableStr += "<th>f(" + i + ")</th>";
 	}
 
-	tableStr += "</tr><tr id=\"numbers\">";
+	tableStr += "</tr><tr class=\"numbers\">";
 
 	for(var i = 0; i < fibSequence.length; i++) {
 		tableStr += "<td>" + fibSequence[i] + "</td>";
@@ -95,11 +114,15 @@ function createTable() {
 	// end of table	
 	tableStr += "</tr></table></div>";
 
+	// insert table html
 	document.getElementById("fibTable").innerHTML = tableStr;
 
-	tableNums = document.querySelectorAll("#numbers td");
+	tableNums = document.querySelectorAll(".numbers td");
 }
 
+/**
+ * goes through fibonacci sequence highlighting cells
+ */
 function teachTable() {
 	var i = 0;
 	var n1 = 0;
@@ -133,6 +156,11 @@ function teachTable() {
 	}, 1500);
 }
 
+/**
+ * stops the timer
+ * 
+ * @param {*} timeInterval 
+ */
 function stopTeaching(timeInterval) {
 	clearInterval(timeInterval);
 }
